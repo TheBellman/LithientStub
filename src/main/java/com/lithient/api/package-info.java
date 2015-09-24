efa11b6b0b0839c43773702b738c8bdbbf2322e6
Robert Hook <rahook@gmail.com>
2015-09-24 15:11:40 +0100

    address checkstyle issues.

---
+++ src/main/java/com/lithient/api/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * classes to provide the exposed API.
+ * 
+ * @author R. Hook
+ * @version 1.0
+ */
+
+package com.lithient.api;
---
+++ src/main/java/com/lithient/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * classes to provide a central controller.
+ * 
+ * @author R. Hook
+ * @version 1.0
+ */
+
+package com.lithient.controller;
